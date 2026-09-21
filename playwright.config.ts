4ff85b982f69e0c7ee5f29d3c99731e0d5c0ebab
Samuel Ventura <samuelveva19@icloud.com>
2026-09-20 20:26:02 -0600

    chore: Configuracion inicial

---
+++ playwright.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from '@playwright/test';
+import * as dotenv from 'dotenv';
+import path from 'path';
+
+dotenv.config({ path: path.resolve(__dirname, '.env') });
+
+export default defineConfig({
+  testDir: './tests',
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  reporter: [
+    ['html', { open: 'never' }],
+    ['list']
+  ],
+  use: {
+    baseURL: process.env.BASE_URL || 'https://api.restful-api.dev',
+    extraHTTPHeaders: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+    },
+  },
+});
